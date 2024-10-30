a015f0fe9e3056f9e6fbb6a572e462e306b6c321
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2024-10-30 15:04:59 +0545

    Experience added

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,8 +1,14 @@
 export default [
     {
         company: "One Point Financial Services",
+        title: "Junior .NET Developer",
+        duration: 'Nov 2024 - Present',
+        details: []
+    },
+    {
+        company: "One Point Financial Services",
         title: "ASP.NET Intern",
-        duration: 'June 2024 (Present)',
+        duration: 'June 2024 - Oct 2024',
         details: [
             '- Creating APIs for banking and financial institutions.',
             '- Developing and managing robust DLLs for API Management System.',
