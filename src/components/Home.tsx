0b5eae7844888b53def1b11b92446b5043e77ab3
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2024-12-09 17:24:42 +0545

    Format adjusted

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,19 +4,19 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 const Home: React.FC = () => {
   return (
-    <div className="h-screen flex flex-col justify-center mx-auto max-w-[1300px] px-5">
-      <div className="main-container h-4/5 rounded-3xl bg-zinc-700 flex flex-row overflow-hidden">
+    <div className="h-screen flex items-center justify-center mx-auto max-w-[1300px] px-6">
+      <div className="main-container h-4/5 rounded-3xl bg-zinc-600 flex flex-row overflow-hidden">
         {/* Left Section */}
-        <div className="border border-red-600 w-1/3">
-          <div className="flex flex-col items-center py-14">
+        <div className="w-1/3">
+          <div className="flex flex-col items-center py-16">
             {/* Image, Name, and Designation */}
             <img
               src="https://via.placeholder.com/150"
               alt="Profile Placeholder"
-              className="rounded-full w-40 h-40 border-4 border-white overflow-hidden"
+              className="rounded-full min-w-44 border-4 border-white overflow-hidden"
             />
-            <h1 className="text-3xl font-bold text-white mt-7">Sujal Maharjan</h1>
-            <p className="text-base text-gray-300 mt-2">Software Engineer</p>
+            <h1 className="text-4xl font-bold text-white mt-7">Sujal Maharjan</h1>
+            <p className="text-lg text-gray-300 mt-2">Software Engineer</p>
 
             {/* Links */}
             <div className="flex mt-4 space-x-4">
@@ -36,16 +36,18 @@ const Home: React.FC = () => {
               </a>
             </div>
 
-            <button className='text-sm text-white border border-white px-5 py-3 rounded-full mt-4'>
+            <button className='text-sm text-white border border-white px-9 py-3 rounded-full mt-12'>
               Download CV
             </button>
           </div>
         </div>
 
         {/* Right Section */}
-        <div className="border border-green-600 w-2/3">
-          <div className="flex flex-col items-center py-14">
-            ads
+        <div className="bg-zinc-900 rounded-l-3xl w-2/3 text-white">
+          <div className="px-10 py-16 w-full h-full flex flex-col items-center justify-center">
+            <h1 className='text-5xl font-bold'>Sujal Maharjan</h1>
+            <p className='text-gray-300 text-xl mt-4'>Software Engineer</p>
+            Use owl carousel to put 2-3 title like software engineer, full stack developer, etc.
           </div>
         </div>
       </div>
